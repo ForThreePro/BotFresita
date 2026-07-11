18412dab832c7dcc3dbbf05ece46ae53ca5e342b
YALLICO OWNER <yallico2024@gmail.com>
2026-07-11 10:28:43 -0500

    Actualizar sticker-s.js

--- plugins/sticker-s.js
+++ plugins/sticker-s.js
@@ -3,12 +3,25 @@ import { sticker } from '../lib/sticker.js'
 let handler = async (m, { conn, usedPrefix, command }) => {
     let q = m.quoted ? m.quoted : m
     let mime = (q.msg || q).mimetype || q.mediaType || ''
-    if (!/webp|image|video/g.test(mime)) return m.reply(`⚡ *RAYO PREM* ➔ Responde a una *imagen, video o gif* para convertirlo en sticker.`) // Cambiado
+    if (!/webp|image|video/g.test(mime)) return m.reply(`╭─❒ *『 𝗧𝗘𝗔𝗠 𝗡𝗜𝗚𝗛𝗧𝗪𝗜𝗦𝗛 』* ❒
+│ 🎭 *STICKER*
+│
+│ ⚠️ *Responde a una imagen, video o gif*
+│ 🌙 *Para convertirlo en sticker*
+╰─────────────────❒`)
+    
     let img = await q.download()
-    let stiker = await sticker(img, false, 'Team Nightwish', 'Whois Yallico') // Cambiado marca
-    conn.sendFile(m.chat, stiker, 'sticker.webp', '⚡ *Rayo Prem* | Sticker creado', m) // Cambiado caption
+    let stiker = await sticker(img, false, 'Team Nightwish', 'Whois Yallico')
+    
+    await conn.sendFile(m.chat, stiker, 'sticker.webp', `╭─❒ *『 𝗧𝗘𝗔𝗠 𝗡𝗜𝗚𝗛𝗧𝗪𝗜𝗦𝗛 』* ❒
+│ ⚡ *STICKER CREADO*
+│
+│ > *El trueno en forma de sticker*
+╰─────────────────❒`, m)
 }
+
 handler.help = ['s']
 handler.tags = ['sticker']
 handler.command = ['s', 'sticker', 'stiker']
+
 export default handler
