b13030216d0735ac36c45e436983aaefa8fb1c1c
YALLICO OWNER <yallico2024@gmail.com>
2026-07-12 13:15:15 -0500

    Actualizar owner-reset.js

--- plugins/owner-reset.js
+++ plugins/owner-reset.js
@@ -4,11 +4,11 @@ var handler = async (m, { conn, isROwner, text }) => {
 
 if (!process.send) throw 'Dont: node mini.js\nDo: node index.js'
 if (conn.user.jid == conn.user.jid) {
-await conn.reply(m.chat, `╭─❒ *『 𝗧𝗘𝗔𝗠 𝗡𝗜𝗚𝗛𝗧𝗪𝗜𝗦𝗛 』* ❒
+await conn.reply(m.chat, `╭─🍓 *『 𝐅𝐑𝐄𝐒𝐈𝐓𝐀 𝐁𝐎𝐓 』* 🍓
 │ 🔄 *REINICIANDO BOT*
 │
-│ ⚡ *Estado:* Reiniciando sistemas
-│ 🌙 *El trueno volverá en segundos*
+│ 🍰 *Estado:* Reiniciando sistemas
+│ 🌸 *La casita volverá en segundos*
 ╰─────────────────❒`, m)
 process.send('reset')
 } else throw 'eh'
